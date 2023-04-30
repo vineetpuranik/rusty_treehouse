8bc334c08dc3e0a5be5df53951a56a17f3bb4ec2
rust <vnt.manu@gmail.com>
2023-04-30 09:34:08 -0700

    moving capturing user input to its own function

--- treehouse/src/main.rs
+++ treehouse/src/main.rs
@@ -1,17 +1,28 @@
 use std::io::stdin;
 
-fn main() {
-    //ask the user their name
-    println!("Hello, what is your name ?");
-
+//function to capture user name and return it
+fn what_is_your_name() -> String {
     //declare a mutable string to store the name of the user
     let mut your_name = String::new();
 
-    //read user input
+    //capture user input in our mut variable your_name
+    //read_line is borrowing the reference to the mutable variable your_name so that it can update it
     stdin()
         .read_line(&mut your_name)
-        .expect("Failed to read line");
+        .expect("Failed to read user input");
+
+    //return the name
+    your_name
+}
+
+fn main() {
+    //ask the user their name
+
+    println!("Hello, what is your name ?");
+
+    //invoke what_is_your_name
+    let name = what_is_your_name();
 
     //print users name
-    println!("Hello, {}", your_name);
+    println!("Hello, {}", name);
 }
